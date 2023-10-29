83925c72a4b1afa5dd3119309304559b066b64ba
Stoyan Shukerov <stoichik@gmail.com>
2023-10-29 13:12:55 -0700

    General style fixes
    - change background to light blue
    - add a logo/main heading
    - fix app html title

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "forecaster/search_locations", to: "forecaster#search_locations", as: "search_locations"
+
   # Defines the root path route ("/")
   root "forecaster#index"
 end
